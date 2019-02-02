29f5e21171e70620912a0ef2054c9a71b643ad18
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 13:32:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,7 +22,7 @@ def won?(board)
     position_1 = board[index_1]
     position_2 = board[index_2]
     position_3 = board[index_3]}
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
 end
