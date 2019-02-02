17a5e64de227daae63a7ed2d5021f59d8700d82a
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 11:10:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 def won?(board)
   if board.select do |combanations|
-    combanations.[
+    combanations.([
       [0,1,2],
       [3,4,5],
       [6,7,8],
@@ -25,7 +25,7 @@ def won?(board)
       [2,5,8],
       [0,4,8],
       [2,4,6]
-    ]
+    ])
     return true
   else
     return false
