315800e02f2afbc539207bacef153ff8db5439bd
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 13:35:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,9 @@ WIN_COMBINATIONS = [
 ]
 def won?(board)
     WIN_COMBINATIONS.each {|win_combo|
-    index_1 = win_combo[0]
-    index_2 = win_combo[1]
-    index_3 = win_combo[2]
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
     position_1 = board[index_1]
     position_2 = board[index_2]
     position_3 = board[index_3]}
