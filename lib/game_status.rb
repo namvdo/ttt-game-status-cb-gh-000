33025e3de19ee0683fa9ccc8cb5dcdcd1d9817d6
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 13:49:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,8 @@ def won?(board)
   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
+  else
+    return false
   }
 end
 def full?(board)
