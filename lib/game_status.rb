fbedafc61b0d95af85ad99c8b288d68cb8054019
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 11:08:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,20 +14,19 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
-def won?
-for each win_combination in WIN_COMBINATIONS do
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
+def won?(board)
+  if board.select do |combanations|
+    combanations.[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]?
+    return true
   else
-    false
-  end
-end
+    return false
 end
