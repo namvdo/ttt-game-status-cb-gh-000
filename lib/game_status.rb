57dc479b7c441be88433e60d7acecd48d3d73a93
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-02 11:04:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 def won?
-for each win_combination in WIN_COMBINATIONS do 
+for each win_combination in WIN_COMBINATIONS do
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
@@ -25,7 +25,7 @@ for each win_combination in WIN_COMBINATIONS do
   position_3 = board[win_index_3]
 
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination 
+    return win_combination
   else
     false
   end
